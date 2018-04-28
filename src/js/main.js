a12ac8edd2d4cc7a9d303d53607007aea6ee0794
AleksandrPrime <golem007@gmail.com>
2018-04-28 15:51:17 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,19 @@ $('#check-box').click(function () {
         $('#data-2').prop('disabled', true);
     }
 });
+
+let nav = $('.menu__nav');
+
+$('.menu__btn').click(function () {
+    $(nav).slideToggle(400);
+    $('.menu__nav').css('display', 'block');
+
+});
+
+$('.search-form__ex').on('click',function(e){
+    if($('.search-form__ex--imp-1').click) {
+        $("#depart").val($(this).text());
+    } else if ($('.search-form__ex--imp-2').click){
+        $("#arriv").val($(this).text());
+    }
+});
